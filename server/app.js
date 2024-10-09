9fc75291f718bc61d6d5f85fdd75c5fc175e7b0b
cmabdulkareem <cmabdulkareem@gmail.com>
2024-10-09 23:17:44 +0530

    initial

--- server/app.js
+++ server/app.js
@@ -5,9 +5,7 @@ import cors from 'cors'
 import session from 'express-session'
 import cookieparser from 'cookie-parser'
 import fileUpload from 'express-fileupload'
-
 import userRouter from './routes/userRoutes.js'
-
 dotenv.config()
 
 
@@ -19,7 +17,6 @@ const corsOptions = {
       method: "GET,POST,HEAD,PUT,PATCH,DELETE",
       credentials: true,
       allowedHeader: "Content-Type, Authorization"
-
 }
 app.use(cors(corsOptions))
 app.use(express.static("public"))
@@ -28,10 +25,13 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookieparser())
 app.use(session({
-          secret: "secret",
-          resave: false,
-          saveUninitialized:true,
-          cookie: {secure: false}
+  secret: 'secret',
+  resave: false,
+  saveUninitialized : false,
+  cookie: {
+      secure: false,
+      maxAge: 1000 * 60 * 60 * 24
+  }
 }))
 
 // app.options('*', cors())
