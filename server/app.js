b62662d1956cc17a45ed1ccccf39c03b75b96c26
cmabdulkareem <cmabdulkareem@gmail.com>
2024-10-10 00:11:38 +0530

    initial

--- server/app.js
+++ server/app.js
@@ -10,10 +10,10 @@ dotenv.config()
 
 
 const app =express()
-const PORT =process.env.PORT
+const PORT = process.env.PORT || 3000
 
 const corsOptions = {
-      origin:"http://localhost:5173",
+      origin:"https://verceltest-two-olive.vercel.app",
       method: "GET,POST,HEAD,PUT,PATCH,DELETE",
       credentials: true,
       allowedHeader: "Content-Type, Authorization"
