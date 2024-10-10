b7c025104bab2f9d3eae1341ae04d1bb27886273
cmabdulkareem <cmabdulkareem@gmail.com>
2024-10-10 08:56:28 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 
 const app =express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 10000 
 
 const corsOptions = {
       origin:"https://verceltest-navy-xi.vercel.app",
